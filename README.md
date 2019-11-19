4403838cafa061a4e2aecd6913d8c3582ac78a8b
Nona07 <modplus07@gmail.com>
2019-11-19 22:43:51 +0900

    最新のgulpの方法

---
+++ README.md
@@ -0,0 +1,22 @@
+gulpをプロジェクトごとにインストールする
+
+https://ics.media/entry/3290
+
+package.jsonファイルの作成
+npm init -y
+
+Gulpのインストール
+
+npm install -D gulp
+
+npm install gulp-sass --save-dev
+npm install gulp-postcss --save-dev
+npm install autoprefixer --save-dev
+npm install gulp-plumber --save-dev
+npm install gulp-changed --save-dev
+npm i browser-sync --save
+npm install gulp-sourcemaps --save-dev
+
+npm install gulp-pug--save-dev
+npm install gulp-rename--save-dev
+npm install pug-php-filter--save-dev
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const 
+gulp = require('gulp'),
+sass = require('gulp-sass'),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+plumber = require('gulp-plumber'),
+changed = require('gulp-changed'),
+sourcemaps = require('gulp-sourcemaps'),
+browserSync = require('browser-sync'),
+pug  = require('gulp-pug'),
+rename  = require('gulp-rename');
+pugPHPFilter	= require('pug-php-filter');
+
+// sass
+gulp.task('sass', () => {
+  gulp.watch('./scss/**/*.scss', () => {
+  // scssファイルからcssファイルを書き出し
+  return gulp.src("./scss/style.scss")
+    .pipe(sourcemaps.init())         // ソースマップ出力のための初期準備
+    .pipe(plumber())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./map')) // ソースマップを出力する
+    .pipe(gulp.dest('./css'));
+  });
+});
+
+//autoprefixer
+gulp.task("autoprefixer",  () => {
+  return gulp.src("./css/style.css")
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ["last 4 versions", "ie >= 11", "iOS >= 8" , "Android >= 4"],
+        cascade: false
+      })
+    ]))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task( 'pug', () => {
+  let option = {
+		pretty: true,
+		filters: {
+			php: pugPHPFilter
+		}
+	}
+  return gulp
+    .src([ 'pug/**/*.pug', '!pug/**/_*.pug' ])
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(rename({
+      extname: '.php'
+    }))
+   .pipe( gulp.dest( './' ) );
+});
+
+gulp.task('watch', () => {
+
+  // scssファイルが変更されたらsassタスクを実行
+  gulp.watch("./scss/**/*.scss", gulp.task('sass'));
+  gulp.watch("./scss/**/*.scss", gulp.task('autoprefixer'));
+  // pugファイルが変更されたらpugタスクを実行
+  gulp.watch([ 'pug/**/*.pug', '!pug/**/_*.pug' ], gulp.task('pug'));
+  // phpファイルとcssファイルが変更されたら、ブラウザをリロード
+  gulp.watch(['./*.html', './*.php', './css/*.css', './js/*.js']).on('change', browserSync.reload);
+
+});
+
+gulp.task('serve', () => {
+  // WordPress開発環境と連携のためプロキシを使用
+  browserSync.init({
+      proxy: "localhost/projectFolder/"
+  });
+});
+
+gulp.task('default', gulp.parallel('serve', 'watch'));
