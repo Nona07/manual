e3109763649d9a27328206fa2fc1e8cf01147f53
Nona07 <modplus07@gmail.com>
2019-12-02 21:28:35 +0900

    色々整理した

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
-const 
-gulp = require('gulp'),
-sass = require('gulp-sass'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-plumber = require('gulp-plumber'),
-sourcemaps = require('gulp-sourcemaps'),
-browserSync = require('browser-sync'),
-pug  = require('gulp-pug'),
-rename  = require('gulp-rename'),
-pugPHPFilter	= require('pug-php-filter');
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      plumber = require('gulp-plumber'),
+      sourcemaps = require('gulp-sourcemaps'),
+      browserSync = require('browser-sync'),
+      pug = require('gulp-pug'),
+      rename = require('gulp-rename'),
+      pugPHPFilter = require('pug-php-filter');
 
 // sass
 gulp.task('sass', () => {
   gulp.watch('./scss/**/*.scss', () => {
   // scssファイルからcssファイルを書き出し
   return gulp.src('./scss/style.scss')
-    .pipe(sourcemaps.init())         // ソースマップ出力のための初期準備
+    .pipe(sourcemaps.init()) //ソースマップ出力のための初期準備
     .pipe(plumber())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(sourcemaps.write('./map')) // ソースマップを出力する
+    .pipe(sourcemaps.write('./map')) //ソースマップを出力する
     .pipe(gulp.dest('./css'));
   });
 });
 
 //autoprefixer
-gulp.task('autoprefixer',  () => {
+gulp.task('autoprefixer', () => {
   return gulp.src('./css/style.css')
     .pipe(postcss([
       autoprefixer({
-        browsers: [ 'last 4 versions', 'ie >= 11', 'iOS >= 8' , 'Android >= 4'],
+        browsers: ['last 4 versions', 'ie >= 11', 'iOS >= 8' , 'Android >= 4'],
         cascade: false
       })
     ]))
   .pipe(gulp.dest('./css'));
 });
 
+//pug
 gulp.task('pug', () => {
   let option = {
 		pretty: true,
@@ -43,16 +45,15 @@ gulp.task('pug', () => {
 		}
 	}
   return gulp
-    .src([ 'pug/**/*.pug', '!pug/**/_*.pug' ])
-    .pipe(pug({
-      pretty: true
-    }))
+    .src(['pug/**/*.pug','!pug/**/_*.pug'])
+    .pipe(pug(option))
     .pipe(rename({
       extname: '.php'
     }))
    .pipe( gulp.dest( './' ) );
 });
 
+//ファイルに変更があったらする処理諸々
 gulp.task('watch', () => {
   // scssファイルが変更されたらsassタスクを実行
   gulp.watch('./scss/**/*.scss', gulp.task('sass'));
